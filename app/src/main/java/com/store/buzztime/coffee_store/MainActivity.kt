11fe225f611d9d5971cf86eea1b8939cae84cb2a
jigang Sun <sjgtongji@126.com>
2017-06-16 15:13:02 +0800

    add some image resource

--- app/src/main/java/com/store/buzztime/coffee_store/MainActivity.kt
+++ app/src/main/java/com/store/buzztime/coffee_store/MainActivity.kt
@@ -1,39 +1,56 @@
 package com.store.buzztime.coffee_store
 
-import android.content.Intent
 import android.databinding.DataBindingUtil
-import android.databinding.ViewStubProxy
-import android.databinding.adapters.ViewStubBindingAdapter
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import com.store.buzztime.coffee_store.Bean.User
 import com.store.buzztime.coffee_store.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : BaseActivity() , View.OnClickListener {
-    var text : String = "";
-    var user : User = User();
-    lateinit var dataBind : ActivityMainBinding;
+class MainActivity : BaseActivity() , View.OnClickListener{
+
     override fun onClick(v: View?) {
         when(v!!.id){
-            R.id.tv_main -> {
-                Log.d(TAG , "onClick")
-                user.name = "world";
-                user.notifyChange();
+            R.id.btn_login -> {
+                var name : String = et_name.text.toString();
+                var password : String = et_password.text.toString();
+                // TODO login
             }
             else -> {}
         }
     }
 
+    var user : User = User();
+    lateinit var dataBind : ActivityMainBinding;
+
     override fun initViews() {
-        tv_main.setText("hello")
+
     }
 
     override fun initEvents() {
-        tv_main.setOnClickListener(this);
+
+        btn_login.setOnTouchListener(View.OnTouchListener { v, event ->
+            when(event.action){
+                MotionEvent.ACTION_DOWN -> {
+                    btn_login.setTextColor(resources.getColor(R.color.text_yellow));
+                }
+                MotionEvent.ACTION_UP -> {
+                    btn_login.setTextColor(resources.getColor(R.color.white))
+                }
+                else -> {}
+            }
+            false;
+        })
+
+        btn_login.setOnClickListener(this)
+
     }
 
     override fun initDatas(view : View) {
